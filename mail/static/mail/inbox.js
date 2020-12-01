9acb09bbd9984148e3b9cef587a5a00fe0a39315
stefanoroque <stefanoroque98@gmail.com>
2020-12-01 12:07:58 -0500

    added archive and unarchive functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // Other events
   document.querySelector('#compose-form').onsubmit = send_email;
+  
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -70,6 +73,10 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#single-email-view').style.display = 'none';
 
+  // // Delete all old emails before repopulating the mailbox
+  // var node = document.getElementById('emails-view');
+  // node.querySelectorAll('*').forEach(n => n.remove());
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
@@ -97,8 +104,24 @@ function load_mailbox(mailbox) {
       email.innerHTML = `<b>To:</b> ${contents.recipients} | <b>Subject:</b> ${contents.subject} | <b>Sent on:</b> ${contents.timestamp}`;
       // Show all white backgrounds on every email
       email.className = email.className + " email-unread"
+    } else if (mailbox == 'inbox') {
+      // we are showing the inbox 
+      if (contents.archived == true) {
+        // Email has been archived, do not display
+        return;
+      }
+      if (contents.read == true) {
+        email.className = email.className + " email-read"
+      } else {
+        email.className = email.className + " email-unread"
+      }
+      email.innerHTML = `<b>From:</b> ${contents.sender} | <b>Subject:</b> ${contents.subject} | <b>Sent on:</b> ${contents.timestamp}`;
     } else {
-      // we are showing the inbox or archived
+      // We are showing archived
+      if (contents.archived == false) {
+        // Email has not been archived, do not display
+        return;
+      }
       if (contents.read == true) {
         email.className = email.className + " email-read"
       } else {
@@ -124,6 +147,9 @@ function view_email(email_id, mailbox) {
           // Mark email as read
           read: true
       })
+    })
+    .catch(error => {
+      console.log('Error:', error)
     });
   }
 
@@ -163,5 +189,69 @@ function view_email(email_id, mailbox) {
       document.querySelector('#timestamp').innerHTML = 'Sent on: ' + timestamp;
       document.querySelector('#body').innerHTML = body;
 
+      // Remove any button that was previously placed in DOM
+      if (document.getElementById('archive-btn') != null) {
+        document.getElementById('archive-btn').remove();
+      }
+
+      // Add buttons to archive or unarchive an email
+      if (mailbox == 'inbox') {
+        // We are in the inbox, so we would want the option to archive
+        const archive_btn = document.createElement('button');
+        archive_btn.className = 'btn btn-secondary';
+        archive_btn.id = 'archive-btn'
+        archive_btn.innerHTML = 'Archive Email';
+
+        // Add email to DOM
+        document.querySelector('#single-email-view').appendChild(archive_btn);
+        archive_btn.addEventListener('click', () => archive_unarchive(email.id, mailbox));
+
+
+      } else if (mailbox == 'archive') {
+        // We are in the archived mailbox, so we would want the option to unarchive
+        const archive_btn = document.createElement('button');
+        archive_btn.className = 'btn btn-secondary';
+        archive_btn.id = 'archive-btn'
+        archive_btn.innerHTML = 'Unarchive Email';
+
+        // Add email to DOM
+        document.querySelector('#single-email-view').appendChild(archive_btn);
+        archive_btn.addEventListener('click', () => archive_unarchive(email.id, mailbox));
+
+      }
+
+  })
+  .catch(error => {
+    console.log('Error:', error)
   });
+}
+
+function archive_unarchive (email_id, mailbox) {
+  let arch;
+
+  if (mailbox == 'inbox') {
+    // We want to archive the email
+    arch = true;
+  } else {
+    // We are in the archived mailbox, so we want to unarchive
+    arch = false;
+  }
+
+  // Send request to API
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: arch
+    })
+  })
+  .catch(error => {
+    console.log('Error:', error)
+  });
+
+  // Bring user back to their inbox
+  load_mailbox('inbox');
+
+  // Need to reload page so that the mailbox properly updates and has the correct emails in it (Kind of a work around)
+  // I think the browser was cacheing old information
+  location.reload();
 }
